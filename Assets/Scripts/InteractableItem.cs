1fe1818ca9133c34226109c9240605af77e8651d
Your Name <kyriann.guede@epita.fr>
2025-03-23 18:44:27 -0600

    Configured Interactable Items and added teleportation areas

--- Assets/Scripts/InteractableItem.cs
+++ Assets/Scripts/InteractableItem.cs
@@ -5,7 +5,7 @@ public class InteractableItem : MonoBehaviour
 {
     // A définir dans l'inspector de unity pour chaque objet dont on veut intéragir avec
     public string itemName;
-    public bool isAnomoly;
+    public bool isAnomaly;
     private bool isHighlighted = false;
 
 
