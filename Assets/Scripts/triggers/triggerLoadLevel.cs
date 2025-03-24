367ccf51dd372a81a2c166b76c8b344911098bba
arnaud kersual <arnaud.kersuall@gmail.com>
2025-03-24 15:34:14 -0400

    Added a way of activating triggers with VR [V0.0.3]

--- Assets/Scripts/triggers/triggerLoadLevel.cs
+++ Assets/Scripts/triggers/triggerLoadLevel.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.Events;
 
 public class triggerLoadLevel : MonoBehaviour
 {
     public string sceneName;
     private GameManager gameManager;
 
-    private void OnTriggerEnter(Collider other) {
-        if(other.CompareTag("Player"))
+    private void OnTriggerEnter(Collider other)
+    {
+        print("TESTTTT");
+        if (other.CompareTag("Player"))
         {
-            gameManager.loadLevel(sceneName);
+            Debug.Log("Player entered the trigger zone!");
+            // Put your logic here (e.g., load scene, open door)
         }
-        
-    } 
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player exited the trigger zone!");
+        }
+    }
 }
